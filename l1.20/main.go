9b4b5150ebf93c81824527187bb3a7c46b05cdc8
Gleb <dawgkek@mail.ru>
2025-08-16 11:00:18 +0300

    added l1.20

--- l1.20/main.go
+++ l1.20/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "snow dog sun"
+
+	words := strings.Fields(str)
+
+	for i := 0; i < len(words); i++ {
+		fmt.Printf("%s ", words[len(words)-1-i])
+	}
+
+}
