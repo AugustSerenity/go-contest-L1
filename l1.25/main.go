f2fbe05c1405ec67fc0c33c6e01584e3312e3723
Gleb <dawgkek@mail.ru>
2025-08-19 12:08:18 +0300

    added l1.25

--- l1.25/main.go
+++ l1.25/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func mySleep(n time.Duration) {
+	end := time.Now().Add(n)
+	for time.Now().Before(end) {
+
+	}
+}
+
+func main() {
+	var n time.Duration
+	fmt.Println("enter time in seconds")
+	fmt.Scan(&n)
+
+	timeNow := time.Now()
+
+	mySleep(n * time.Second)
+
+	timeAfter := time.Since(timeNow)
+	fmt.Println(timeAfter)
+}
